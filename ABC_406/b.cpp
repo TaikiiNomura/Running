f7faba0241291f0dde3cf24b4a8ab1a12597ac9b
TaikiiNomura <23b2119@sit.shonan-it.ac.jp>
2025-06-14 19:57:55 +0900

    Update b.cpp

--- ABC_406/b.cpp
+++ ABC_406/b.cpp
@@ -4,12 +4,17 @@ int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
-    int N,K,ans=1;cin>>N>>K;
+    int N,K;
+    long long ans=1,y=1;
+    cin>>N>>K;
+    for(int i=0;i<K;i++)y*=10;
+    y--;
     for(;N--;)
     {
-        int a;cin>>a;
-        ans*=a;
-        if(to_string(ans).size()>K)ans=1;
+        long long a;
+        cin>>a;
+        if(ans>y/a)ans=1;
+        else ans*=a;
     }
     cout<<ans<<endl;
     return 0;
